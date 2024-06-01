f7b9494c95c596ab8803adda66be12820b9bdda2
Ben Blank <ben.blank@gmail.com>
2024-06-01 13:16:10 -0700

    xdg-icon-resource: add support for SVG icons

--- autotests/t-xdg-icon-resource.sh
+++ autotests/t-xdg-icon-resource.sh
@@ -7,3 +7,15 @@ touch $LABDIR/icon.png
 run generic xdg-icon-resource install --mode system --size 256 \
     $LABDIR/icon.png myvendor-myapp
 assert_file_exists $XDG_DATA_DIR_LOCAL/icons/hicolor/256x256/apps/myvendor-myapp.png
+
+test_that_it installs an svg icon system-wide
+touch $LABDIR/icon.svg
+run generic xdg-icon-resource install --mode system --size scalable \
+    $LABDIR/icon.svg myvendor-myapp
+assert_file_exists $XDG_DATA_DIR_LOCAL/icons/hicolor/scalable/apps/myvendor-myapp.svg
+
+test_that_it installs an xpm icon system-wide
+touch $LABDIR/icon.xpm
+run generic xdg-icon-resource install --mode system --size 256 \
+    $LABDIR/icon.xpm myvendor-myapp
+assert_file_exists $XDG_DATA_DIR_LOCAL/icons/hicolor/256x256/apps/myvendor-myapp.xpm
